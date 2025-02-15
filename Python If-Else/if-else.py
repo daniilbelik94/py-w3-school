5e3222a6a28e8bee3ce6b658e87ead659e9c0da7
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2025-02-15 20:57:58 +0100

    add while loop examples and enhance if-else script with comments

--- Python If-Else/if-else.py
+++ Python If-Else/if-else.py
@@ -103,7 +103,7 @@ print("A") if a > b else print("B")
 
 a = 330
 b = 330
-print("A") if a > b else print("=") if a == b else print("B")
+print("A") if a > b else print("=") if a == b else print("B") # returns "="
 
 
 # And
